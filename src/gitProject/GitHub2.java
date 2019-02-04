de0629f53563e77db284f4982a78001e0dcde4f9
Sheila <qa4sheila@gmail.com>
2019-02-04 08:57:25 -0600

    Second Project

---
+++ src/gitProject/GitHub2.java
@@ -0,0 +1,12 @@
+package gitProject;
+
+public class GitHub2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("how are you ?");
+
+	}
+
+}
